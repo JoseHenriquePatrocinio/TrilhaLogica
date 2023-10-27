1267aab9a3270816abdf2d8d5a710f6a74db636a
JoseHenriquePatrocinio <henriquepatrocinio98@gmail.com>
2023-10-27 20:49:16 -0300

    codicional

---
+++ Condicional/ConsoleApp4/Desafio4.cs
@@ -0,0 +1,76 @@
+﻿using ConsoleApp4;
+using System;
+using System.Data;
+
+class Desafio3
+{
+    static void Main()
+    {
+
+        Console.Write("Digite 1(Filme), 2(Série), 3(Documentário): ");
+        int escolha = int.Parse(Console.ReadLine());  
+        dto escolhas = new dto();
+
+        if (escolha == 1)
+        {
+            escolhas.Categoria = "Filme";
+
+            Console.Write("Digite 1(Drama), 2(Ação)");
+            escolha = int.Parse(Console.ReadLine());
+
+            if (escolha == 1)
+            {
+                escolhas.Genero = "Drama";
+                escolhas.Sugestao = "Ex Machina:Instinto Artificial";
+            }
+
+            else
+            {
+                escolhas.Genero = "Ação";
+                escolhas.Sugestao = "Minority Report";
+            }
+        }
+        else if(escolha == 2)
+        {
+            escolhas.Categoria = "Série";
+
+            Console.Write("Digite 1(Suspense), 2(Ficção Cientifica)");
+            escolha = int.Parse(Console.ReadLine());
+
+            if (escolha == 1)
+            {
+                escolhas.Genero = "Suspense";
+                escolhas.Sugestao = "Black Mirror";
+            }
+            else
+            {
+                escolhas.Genero = "Ficção Cientifica";
+                escolhas.Sugestao = "Better than us";
+            }              
+        }
+        else
+        {
+            escolhas.Categoria = "Documentário";
+
+            Console.Write("Digite 1(Tecnologia), 2(Biografia)");
+            escolha = int.Parse(Console.ReadLine());
+
+            if (escolha == 1)
+            {
+                escolhas.Genero = "Tecnologia";
+                escolhas.Sugestao = "Watson da IBM";
+            }
+            else
+            {
+                escolhas.Genero = "Biografia";
+                escolhas.Sugestao = "O código Bill Gates";
+            }                
+        }
+
+        Console.Clear();
+        Console.WriteLine(escolhas.Categoria);
+        Console.WriteLine(escolhas.Genero);
+        Console.WriteLine(escolhas.Sugestao);
+
+    }
+}
---
+++ Condicional/ConsoleApp4/dto.cs
@@ -0,0 +1,15 @@
+﻿namespace ConsoleApp4
+{
+    public class dto
+    {
+
+        private string categoria;
+        private string genero;
+        private string sugestao;
+
+        public string Categoria { get; set; }
+        public string Genero { get; set; }
+        public string Sugestao{ get; set; }
+
+    }
+}
