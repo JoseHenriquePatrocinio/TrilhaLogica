c93f8c9f6055e97eec2521fe2bab9f20ae2198ab
JoseHenriquePatrocinio <henriquepatrocinio98@gmail.com>
2023-10-27 21:15:51 -0300

    refactor

--- Condicional/ConsoleApp1/Desafio1.cs
+++ Condicional/ConsoleApp1/Desafio1.cs
@@ -1,40 +1,25 @@
-﻿using System;
-using System.Data;
-
-class Desafio1
+﻿class Desafio1
 {
     static void Main()
     {
 
-        Console.Write("Digite um numero de 1 a 12: ");
+        Console.Write("Digite 1 para iniciar: ");
         int num = int.Parse(Console.ReadLine());
+        Console.Clear();
 
-        if (num == 0 || num > 12)
-            Console.WriteLine("Numero Inválido");
-        else
-            if (num == 1)
-            Console.WriteLine("Janeiro");
-        else if (num == 2)
-            Console.WriteLine("Fevereiro");
-        else if (num == 3)
-            Console.WriteLine("Março");
-        else if (num == 4)
-            Console.WriteLine("Abril");
-        else if (num == 5)
-            Console.WriteLine("Maio");
-        else if (num == 6)
-            Console.WriteLine("Junho");
-        else if (num == 7)
-            Console.WriteLine("Julho");
-        else if (num == 8)
-            Console.WriteLine("Agosto");
-        else if (num == 9)
-            Console.WriteLine("Setembro");
-        else if (num == 10)
-            Console.WriteLine("Outubro");
-        else if (num == 11)
-            Console.WriteLine("Novembro");
-        else
-            Console.WriteLine("Dezembro");
+        string[] meses = { "Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro" };
+
+        for (int i = 0; i < 12; i++)
+        {
+            if (num != 0 || num < 12)
+            {
+                Console.Write("Digite um numero de 1 a 12: ");
+                num = int.Parse(Console.ReadLine());
+                Console.Clear();
+                Console.WriteLine(meses[num - 1]);
+            }
+            else
+                Console.WriteLine("Valor Inválido");
+        }
     }
 }
